c1df5c22a944d0206abc2ecb53094d8a372114aa
root <root@ltccip8.aus.stglabs.ibm.com>
2020-08-07 15:53:08 +0530

    data collection script changes

--- data_collection.sh
+++ data_collection.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-sleep_time=${1:-60}
-ITER=${2:-1}
-LOG_DIR=${3:-/mmtests/mmtests_CI/work/data_collection}
+sleep_time=${1:-20}
+LOG_DIR=${2:-/mmtests/mmtests_CI/work/data_collection}
+ITER=${3:-1}
 
-echo $LOG_DIR
-exit
+if [ -d "/mmtests/mmtests_CI/work/data_collection" ] 
+then
+	mkdir -p /mmtests/mmtests_CI/work/data_collection
+fi 
 
 sleep $sleep_time
-perf record -a -e cycles -c 10000000 -o /mmtests/mmtests_CI/work/data_collection/perf.raw_$ITER sleep 7 &
-vmstat 1 10 > /mmtests/mmtests_CI/work/data_collection/vmstat_$ITER &
-iostat -Nxmdt  1 10 > /mmtests/mmtests_CI/work/data_collection/iostat_$ITER &
-
+perf record -a -e cycles -c 10000000 -o $LOG_DIR/perf.raw_$ITER sleep 10 &
+vmstat 1 10 > $LOG_DIR/vmstat_$ITER &
+iostat -Nxmdt  1 10 > $LOG_DIR/iostat_$ITER &
+mpstat -P ALL 1 10 >  $LOG_DIR/mpstat_$ITER &
