1c999f46f9b6479f68a5d9234ed59df6c20efe39
root <root@rakshithlinux3.aus.stglabs.ibm.com>
2020-08-11 06:09:13 -0400

    perf path issue2

--- data_collection.sh
+++ data_collection.sh
@@ -9,7 +9,7 @@ then
 fi 
 
 sleep $sleep_time
-perf record -a -g -c 10000000 -o $LOG_DIR/perf.raw.callgraph.dat_$i sleep 10 &
+perf record -a -g -c 10000000 -o $LOG_DIR/perf.raw.callgraph.dat_$ITER sleep 10 &
 vmstat 1 10 > $LOG_DIR/vmstat_$ITER &
 iostat -Nxmdt  1 10 > $LOG_DIR/iostat_$ITER &
 mpstat -P ALL 1 10 >  $LOG_DIR/mpstat_$ITER &
