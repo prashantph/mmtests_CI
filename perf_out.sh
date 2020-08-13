c4197042661f84152e11be4ed19c1f2394bc6836
root <root@rakshithlinux3.aus.stglabs.ibm.com>
2020-08-13 06:35:01 -0400

    perf report script

--- perf_out.sh
+++ perf_out.sh
@@ -1,10 +1,15 @@
 
 
 log_dir=$1
-perf_files=($(ls $log_dir/perf.raw.callgraph.dat*|awk '{print $NF}'|rev |cut -d '/' -f 1|rev))
+perf_dirs=($(ls -l $log_dir | grep ^d | awk '{print $9}')) 
 
-for i in "${perf_files[@]}"
-do 
-	echo $i
-	perf report -n --no-children --sort=dso,symbol -i  $log_dir/$i  > $log_dir/${i}.out
+for k in "${perf_dirs[@]}"
+do
+	perf_files=($(ls $log_dir/$k/logs/perf.raw.callgraph.dat*|awk '{print $NF}'|rev |cut -d '/' -f 1|rev))
+
+	for i in "${perf_files[@]}"
+	do 
+		echo $log_dir/$k/logs/$i
+		perf report -n --no-children --sort=dso,symbol -i  $log_dir/$k/logs/$i  > $log_dir/$k/logs/${i}.out
+	done
 done
