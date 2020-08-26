18b7dde8e43bf0e28476f81cf1cc969bda0d089d
root <root@rakshithlinux3.aus.stglabs.ibm.com>
2020-08-26 02:39:34 -0400

    added few more dependent packages

--- prereq_setup.sh
+++ prereq_setup.sh
@@ -9,12 +9,25 @@ if [ -e /etc/redhat-release ];
 	yum -y install perl*
 	yum -y install patch*
 	yum -y install perl-Time-HiRes
+	yum -y install numactl-devel*
+	yum -y install gnuplot*
+	yum -y install ncurses-devel*
+	yum -y install libxml2-devel*
+	yum -y install boost-devel*
+	yum -y install cmake*
 elif [ -e /etc/SuSE-release ];
   then 
 	zypper install -y time*
 	zypper install -y perl*
 	zypper install -y perl-Time-HiRes
 	zypper install -y patch*
+	zypper install -y perl-Time-HiRes 
+	zypper install -y numactl-devel*
+	zypper install -y gnuplot*
+	zypper install -y ncurses-devel*
+	zypper install -y libxml2-devel*
+	zypper install -y boost-devel*
+	zypper install -y cmake*
 	
                         elif [ -e /etc/os-release ];
                                 then osdetails=`lsb_release -a 2>/dev/null | grep -i 'Description' | awk '{print $2$3;}'`;
